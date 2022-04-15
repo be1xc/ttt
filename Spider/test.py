b13755b333ea317202c3e1dcb57d7bbe54417c05
Xiaoc <83504922+be1xc@users.noreply.github.com>
2022-04-15 15:03:22 +0800

    Update test.py

--- Spider/test.py
+++ Spider/test.py
@@ -33,7 +33,7 @@ import json
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
