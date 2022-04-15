212bf03f56b6fc65309b3d9458732b0b738ec664
Xiaoc <83504922+be1xc@users.noreply.github.com>
2022-04-15 12:41:12 +0800

    Update test.py

--- Spider/test.py
+++ Spider/test.py
@@ -46,7 +46,7 @@ time.sleep(2)
 driver.find_element_by_xpath("/html/body/section/form/button").click()
 # driver.find_element_by_xpath('//*[@id="main"]/section/section/section[2]/div/div[1]/div/ul/section/section/article/p').click()
 try:
-  m = driver.find_element_by_css_selector('#main > section > section > section.app-wrapper > div > div.scrollbar-wrapper.el-scrollbar__wrap > div > ul > section > section > article > p')
+  global m = driver.find_element_by_css_selector('#main > section > section > section.app-wrapper > div > div.scrollbar-wrapper.el-scrollbar__wrap > div > ul > section > section > article > p')
 except:
    print('跳过了异常')
 else:
